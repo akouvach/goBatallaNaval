4c58e24338c0fdb1e08aaa6848fd500bfb691d9e
Andres Kouvach <akouvach@yahoo.com>
2020-07-29 14:17:36 -0300

    gorilla inicial

--- server.go
+++ server.go
@@ -5,7 +5,30 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/akouvach/gobatallanaval/routes"
+	"github.com/gorilla/mux"
 )
+
+// Search se utiliza para buscar algo
+func Search(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Search \n")
+
+}
+
+// Load se utiliza para buscar algo
+func Load(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Load \n")
+}
+
+// Canales se utiliza para buscar algo
+func Canales(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	canal := vars["canal"]
+
+	fmt.Fprintf(w, "Canal: %s \n", canal)
+
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -13,8 +36,17 @@ func main() {
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
-	http.Handle("/", http.FileServer(http.Dir("./static")))
+	// models.Init()
+	r := routes.NewRouter()
 
-	fmt.Printf("Iniciando server en puerto %v \n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	// r.Handle("/", http.FileServer(http.Dir("./static/")))
+	// s := http.StripPrefix("/static/", http.FileServer(http.Dir("./static/")))
+	// r.PathPrefix("/static/").Handler(s)
+	r.HandleFunc("/search/{searchTerm}", Search).Methods("GET")
+	r.HandleFunc("/load/{dataId}", Load).Methods("GET")
+	r.HandleFunc("/canales/{canal}", Canales).Methods("GET")
+
+	http.Handle("/", r)
+
+	http.ListenAndServe(":"+port, nil)
 }
